e6228631b07dd2160a489dcd442a65e65c2dc796
carta87 <cart_albert@hotmail.com>
2022-01-07 12:14:18 -0500

    proyecto spring plazi

---
+++ plazi-market/src/test/java/com/plazimarket/PlaziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plazimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
